d2a9aef75f1a522a002e30c9cadafb8ae9e1d817
Dorel Kalev <158530164+dorelkalev@users.noreply.github.com>
2025-02-23 14:38:08 -0500

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WarGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the War Card Game!");
+            WarGame game = new WarGame();
+            game.PlayGame();
+        }
+    }
+}
