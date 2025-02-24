c3c2b44a955aad95f9aee497f813669d764867ef
Dorel Kalev <158530164+dorelkalev@users.noreply.github.com>
2025-02-24 18:46:45 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -7,7 +7,7 @@ namespace WarGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the War Card Game!");
-            War_Game game = new War_Game();
+            WarGame game = new WarGame();
             game.PlayGame();
         }
     }
